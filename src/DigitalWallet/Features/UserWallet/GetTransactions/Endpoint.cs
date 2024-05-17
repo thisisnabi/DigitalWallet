2756545deac375383716596912edc9897b12edb7
thisisnabi <thisisnabi@outlook.com>
2024-05-17 10:34:28 +0330

    Change suspend wallet endpoint

---
+++ src/DigitalWallet/Features/UserWallet/GetTransactions/Endpoint.cs
@@ -0,0 +1,28 @@
+﻿ namespace DigitalWallet.Features.UserWallet.GetTransactions;
+
+public static class Endpoint
+{
+
+    public static IEndpointRouteBuilder AddGetTransactionsEndPoint(this IEndpointRouteBuilder endpoint)
+    {
+        endpoint.MapGet("/{wallet-id:guid:required}/transactions/",
+            async ([FromRoute(Name = "wallet-id")]Guid Id, WalletDbContext _dbContext, CancellationToken cancellationToken) =>
+        {
+
+            var walletId = WalletId.Create(Id);
+
+            return Results.Ok(_dbContext.Transactions.Where(x => x.WalletId == WalletId)
+                                                     .OrderByDescending(x => x.CreatedOn)
+                                                     .Select(x => new { 
+                                                        CreatedOn = x.CreatedOn,
+                                                        Descripiton = x.Description,
+                                                        Type = x.Type,
+                                                        TypeName = x.Type.ToString(),
+                                                        Kind = x.Kind,
+                                                        KindName = x.Kind.ToString()
+                                                     }));
+        }).WithTags("Transaction");
+        return endpoint;
+    }
+
+}
--- src/DigitalWallet/Features/UserWallet/Suspend/Endpoint.cs
+++ src/DigitalWallet/Features/UserWallet/Suspend/Endpoint.cs
@@ -2,20 +2,17 @@
 
 public static class Endpoint
 {
-
     public static IEndpointRouteBuilder AddSuspendWalletEndpoint(this IEndpointRouteBuilder endpoint)
     {
         endpoint.MapPatch("/{wallet-id:guid:required}/suspend",
-            async ([FromRoute(Name = "wallet-id")] Guid Id,
-            CancellationToken ct,
-            WalletService _service) =>
-        {
-            var walletId = WalletId.Create(Id);
-            await _service.SuspendAsync(walletId, ct);
+            async ([FromRoute(Name = "wallet-id")] Guid Id, WalletService _service, CancellationToken cancellationToken) =>
+            {
+                var walletId = WalletId.Create(Id);
+                await _service.SuspendAsync(walletId, cancellationToken);
 
-            return Results.Ok("Wallet suspended successfully!");
-        }).WithTags("Wallet");
+                return Results.Ok("Wallet suspended successfully!");
+            });
+
         return endpoint;
     }
-
 }
