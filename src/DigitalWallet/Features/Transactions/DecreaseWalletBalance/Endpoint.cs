df1583a0e84bdada78182239f2065fbe97dd6af0
nader.javid <nader.javid@gmail.com>
2024-05-20 12:29:30 +0330

    Fix decrease wallet balance validation

--- src/DigitalWallet/Features/Transactions/DecreaseWalletBalance/Endpoint.cs
+++ src/DigitalWallet/Features/Transactions/DecreaseWalletBalance/Endpoint.cs
@@ -1,4 +1,4 @@
-﻿ namespace DigitalWallet.Features.Transactions.DecreaseWalletBalance;
+﻿namespace DigitalWallet.Features.Transactions.DecreaseWalletBalance;
 
 public static class Endpoint
 {
@@ -8,11 +8,18 @@ public static class Endpoint
         endpoint.MapPost("/{wallet_id:guid:required}/decrease", 
             async ([FromRoute(Name = "wallet_id")]Guid Id, [FromBody]DecreaseWalletBalanceRequest request, TransactionService _service ,CancellationToken cancellationToken) =>
             {
+
+                var validationResult = await new DecreaseWalletBalanceRequestValidator().ValidateAsync(request);
+                if (!validationResult.IsValid)
+                {
+                    return Results.BadRequest(validationResult.Errors);
+                }
+
                 var walletId = WalletId.Create(Id);
                 await _service.DecreaseBalanceAsync(walletId, request.Amount, request.Description, cancellationToken);
 
                 return Results.Ok("Wallet balance decreased successfully!");
-        }).Validator<DecreaseWalletBalanceRequest>();
+        });
         return endpoint;
     }
 
