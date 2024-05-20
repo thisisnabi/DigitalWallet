19f98ce512f0002aa8c49ead65da135846b52367
nader.javid <nader.javid@gmail.com>
2024-05-20 12:30:58 +0330

    Fix increase wallet balance validation

--- src/DigitalWallet/Features/Transactions/IncreaseWalletBalance/Endpoint.cs
+++ src/DigitalWallet/Features/Transactions/IncreaseWalletBalance/Endpoint.cs
@@ -1,18 +1,24 @@
-﻿ namespace DigitalWallet.Features.Transactions.IncreaseWalletBalance;
-
+﻿namespace DigitalWallet.Features.Transactions.IncreaseWalletBalance;
 public static class Endpoint
 {
 
     public static IEndpointRouteBuilder AddIncreaseWalletBalanceEndPoint(this IEndpointRouteBuilder endpoint)
     {
-        endpoint.MapPost("/{wallet_id:guid:required}/increase", 
-            async ([FromRoute(Name = "wallet_id")]Guid Id,[FromBody]IncreaseWalletBalanceRequest request, TransactionService _service ,CancellationToken cancellationToken) =>
+        endpoint.MapPost("/{wallet_id:guid:required}/increase",
+            async ([FromRoute(Name = "wallet_id")] Guid Id, [FromBody] IncreaseWalletBalanceRequest request, TransactionService _service, CancellationToken cancellationToken) =>
             {
+
+                var validationResult = await new IncreaseWalletBalanceRequestValidator().ValidateAsync(request);
+                if (!validationResult.IsValid)
+                {
+                    return Results.BadRequest(validationResult.Errors);
+                }
+
                 var walletId = WalletId.Create(Id);
                 await _service.IncreaseBalanceAsync(walletId, request.Amount, request.Description, cancellationToken);
 
                 return Results.Ok("Wallet balance increased successfully!");
-        }).Validator<IncreaseWalletBalanceRequest>();
+            });
         return endpoint;
     }
 
