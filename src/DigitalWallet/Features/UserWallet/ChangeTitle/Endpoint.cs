e39dbc3c1f8ba329b6f33549a788b3fb8db6ca64
nader.javid <nader.javid@gmail.com>
2024-05-20 12:34:13 +0330

    Fix change wallet title validation

--- src/DigitalWallet/Features/UserWallet/ChangeTitle/Endpoint.cs
+++ src/DigitalWallet/Features/UserWallet/ChangeTitle/Endpoint.cs
@@ -1,4 +1,6 @@
-﻿namespace DigitalWallet.Features.UserWallet.ChangeTitle;
+﻿using DigitalWallet.Features.Transactions.IncreaseWalletBalance;
+
+namespace DigitalWallet.Features.UserWallet.ChangeTitle;
 
 public static class Endpoint
 {
@@ -8,11 +10,18 @@ public static class Endpoint
         endpoint.MapPatch("/{wallet_id:guid:required}",
             async ([FromRoute(Name = "wallet_id")] Guid Id,[FromBody] ChangeTitleRequest request, WalletService _service, CancellationToken cancellationToken) =>
             {
+
+                var validationResult = await new ChangeTitleRequestValidator().ValidateAsync(request);
+                if (!validationResult.IsValid)
+                {
+                    return Results.BadRequest(validationResult.Errors);
+                }
+
                 var walletId = WalletId.Create(Id);
                 await _service.ChangeTitleAsync(walletId, request.Title, cancellationToken);
 
                 return Results.Ok("Wallet title changed successfully!");
-            }).Validator<ChangeTitleRequest>();
+            });
 
         return endpoint;
     }
