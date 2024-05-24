6122e1ff3514338f2bcf9aa642ff21d072b96ed0
Emirhan Aksoy <emirhan.aksoy@outlook.com.tr>
2024-05-24 23:26:21 +0300

    Integration test example with api factory

---
+++ tests/DigitalWallet.IntegrationTests/CurrencyTest/CreateCurrencyEndpointTests.cs
@@ -0,0 +1,40 @@
+﻿
+
+using DigitalWallet.Features.MultiCurrency;
+using DigitalWallet.Features.MultiCurrency.CreateCurrency;
+using DigitalWallet.IntegrationTests.Factory;
+using FluentAssertions;
+using System.Net.Http.Json;
+
+namespace DigitalWallet.IntegrationTests.CurrencyTest;
+
+public class CreateCurrencyEndpointTests : IClassFixture<DigitalWalletApiFactory>
+{
+    private readonly HttpClient _httpClient;
+    public readonly DigitalWalletApiFactory _digitalWalletApiFactory;
+
+    public CreateCurrencyEndpointTests(DigitalWalletApiFactory digitalWalletApiFactory)
+    {
+        _digitalWalletApiFactory = digitalWalletApiFactory;
+        _httpClient = _digitalWalletApiFactory.CreateClient();
+    }
+
+    [Fact(DisplayName = "Create Currency With Invalid Raito")]
+    public async Task Create_Currency_With_Invalid_Ratio()
+    {
+        // Arrange
+        CreateCurrencyRequest request = new("rial", "rial", 0);
+
+
+        // Act
+        HttpResponseMessage httpResponseMessage = await _httpClient.PostAsJsonAsync($"{FeatureManager.Prefix}/", request);
+
+
+        // Assert
+
+        httpResponseMessage.StatusCode.Should().Be(System.Net.HttpStatusCode.UnprocessableEntity);
+
+    }
+
+
+}
---
+++ tests/DigitalWallet.IntegrationTests/Factory/DigitalWalletApiFactory.cs
@@ -0,0 +1,67 @@
+﻿using DigitalWallet.Common.Persistence;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DigitalWallet.IntegrationTests.Factory;
+
+public class DigitalWalletApiFactory : WebApplicationFactory<IAssemblyMarker>, IAsyncLifetime
+{
+          
+
+    public Task InitializeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        // Clear default db context 
+        builder.ConfigureServices(services =>
+        {
+            ServiceDescriptor? descriptor = services.SingleOrDefault(
+                d => d.ServiceType == typeof(DbContextOptions<WalletDbContext>));
+
+            if(descriptor is not null)
+            {
+                services.Remove(descriptor);
+            }
+
+            services.AddDbContext<WalletDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("WalletDbContextTest");
+            });
+
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var testDbContext = scopedServices.GetRequiredService<WalletDbContext>();
+
+                try
+                {
+                    testDbContext.Database.EnsureCreated();
+                    if (!testDbContext.Database.IsInMemory())
+                    {
+                        testDbContext.Database.Migrate();
+                    }
+                    //db.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred applying migrations to the database. Error: {Message}", ex.Message);
+                }
+            }
+        });
+
+        
+    }
+
+    Task IAsyncLifetime.DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+}
